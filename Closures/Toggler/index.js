db8deb00aa1443206bd82ad45d1ab839e5f80b37
shubham-955 <shubhamgadge722@gmail.com>
2022-04-17 01:45:44 +0530

    Added Closures

---
+++ Closures/Toggler/index.js
@@ -0,0 +1,18 @@
+function toggler() {
+    var arr = arguments
+    var index = -1;
+    return function () {
+        index += 1;
+        if (index >= arr.length) {
+            index = 0;
+        }
+        return arr[index];
+    }
+}
+
+const toggle = toggler(1, 2, 3);
+
+console.log(toggle())
+console.log(toggle())
+console.log(toggle())
+console.log(toggle())
